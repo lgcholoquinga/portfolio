6a11824974dde710f1516f4bd509d79bdffc4ed1
lgcholoquinga <ldu-aries.lk@hotmail.es>
2020-03-24 16:19:35 -0500

    Section Congress Finished

--- src/components/Congress.jsx
+++ src/components/Congress.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import '../styles/Congress.css';
+import firebase from '../firebase';
 
 const Congress = () => {
+
+    const [congress,setCongress] = useState([]);
+
+    useEffect(() => {
+        firebase.firestore().collection('courses').onSnapshot(snapshot => {
+            const listCongress = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            setCongress(listCongress);
+        });
+    },[]);
     return (
         <section className="container">
             <h1 className="congress-title">Congress</h1>
+            <div className="row congress-container">
+                {
+                    congress.map((item) => (
+                        <div className="col-12 col-md-6" key={item.id}>
+                            <div className="congress-card">
+                                <div className="congress_card-image">
+                                    <img src={item.logo} alt="congressImage"/>
+                                </div>
+                                <div className="congress_card-details">
+                                    <h2 className='congress_card-title'>{item.title}</h2>
+                                    <h3 className='congress_card-duration'><strong>Duration:</strong> {item.duration}</h3>
+                                    <h3 className='congress_card-city'><strong>City:</strong> {item.city}</h3>
+                                    <h3 className='congress_card-date'><strong>Date:</strong> {item.dateInit} to {item.dateFinish}</h3>
+                                    <a className='congrescard-button' href={item.url} target="_blank" rel="noopener noreferrer">See Certificate</a>
+                                </div>
+                            </div>
+                        </div>  
+                    ))
+                }
+            </div>
         </section>
     );
 };
