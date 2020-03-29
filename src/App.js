00f80ad8223f1f92fb5358614bb2398513fd17be
lgcholoquinga <ldu-aries.lk@hotmail.es>
2020-03-29 12:39:26 -0500

    Section Project Finished

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Hero from './components/Hero';
 import Skill from './components/Skill';
 import Congress from './components/Congress';
 import Footer from './components/Footer';
+import Projects from './components/Project';
 
 function App() {
   return (
     <div className=''>
       <Header />
       <Hero />
+      <Projects />
       <Congress />
       <Skill />
       <Footer />
---
+++ src/components/Project.jsx
@@ -0,0 +1,56 @@
+import React, {useState,useEffect} from 'react';
+import '../styles/Projects.css';
+import firebase from '../firebase';
+
+const Project = () => {
+
+    const [project, setProject] = useState([]);
+
+    useEffect(() => {
+        firebase.firestore().collection('projects').onSnapshot(snapshot => {
+            const lisProjects = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            setProject(lisProjects);
+        });
+    },[]);
+
+    return (
+        <section className='container'>
+            <h2 className='projects-title'>Projects</h2>
+            <div className="row project-container">
+                {
+                    project.map((item) => (
+                        <div className="col-12 col-md-6" key={item.id}>
+                            <div className="project-card">
+                                <div className="project-card-image">
+                                    <a href="/">
+                                        <img src={item.image} alt="project-logo"/>
+                                    </a>
+                                </div>
+                                <div className="project-card-title">
+                                    <p>{item.title}</p>
+                                </div>
+                                <div className="project-card-details">
+                                    <p>{item.description}</p>
+                                    <p className="project-card-sub">
+                                        <strong>
+                                            Skills Learned:
+                                        </strong>
+                                    </p>
+                                    <p>{item.skill}</p>
+                                </div>
+                                <a href={item.url} className="project-url" target="_blank" rel="noopener noreferrer">
+
+                                </a>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </section>
+    );
+};
+
+export default Project;
