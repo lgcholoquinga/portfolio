3f82fdfeb88c02e34e89f647c09c98803171b67d
lgcholoquinga <ldu-aries.lk@hotmail.es>
2020-03-29 15:48:28 -0500

    section a 20%

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section className="container">
+            <h2 className="about-title">About Me</h2>
+            <div className="row about-row">
+                <div className="col about-col">
+                    
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default About;
